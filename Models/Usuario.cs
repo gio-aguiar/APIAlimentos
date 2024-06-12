af30c1586fd011400232b7934ab5ae2c9b5c9076
Giovanna <torresssgio@gmail.com>
2024-06-11 21:44:16 -0300

    first commit

---
+++ Models/Usuario.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using alimentosAPI.Models;
+using Microsoft.Identity.Client;
+
+namespace AlimentosAPI.Models
+{
+    public class Usuario
+    {
+
+        
+        public int Id { get; set;}
+        public string Username { get; set;}
+       
+        public byte[]? PasswordHash {get; set;}
+
+        public byte[]? PasswordSalt {get; set;}
+
+        public DateTime? DataAcesso {get; set;}
+
+        [NotMapped]
+
+        public string PasswordString {get; set;} = String.Empty;
+
+        public List<Alimentos> Alimentos {get; set;} = new List<Alimentos>(); 
+        
+        public string Perfil {get; set;} = string.Empty;
+
+        public string Email {get; set;} = string.Empty;
+    }
+}
---
+++ script01_TabelaAlimentos.sql
@@ -0,0 +1,44 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [TB_Alimentos] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] varchar(200) NOT NULL,
+    [Calorias] int NOT NULL,
+    [Carboidratos] int NOT NULL,
+    [Proteinas] int NOT NULL,
+    [Gorduras] int NOT NULL,
+    [Fibras] int NOT NULL,
+    [Sodio] int NOT NULL,
+    [Tipo] int NOT NULL,
+    CONSTRAINT [PK_TB_Alimentos] PRIMARY KEY ([Id])
+);
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Calorias', N'Carboidratos', N'Fibras', N'Gorduras', N'Nome', N'Proteinas', N'Sodio', N'Tipo') AND [object_id] = OBJECT_ID(N'[TB_Alimentos]'))
+    SET IDENTITY_INSERT [TB_Alimentos] ON;
+INSERT INTO [TB_Alimentos] ([Id], [Calorias], [Carboidratos], [Fibras], [Gorduras], [Nome], [Proteinas], [Sodio], [Tipo])
+VALUES (1, 56, 15, 1, 0, 'Maçã', 0, 0, 1),
+(2, 98, 26, 2, 0, 'Banana', 1, 21, 1),
+(3, 207, 1, 0, 11, 'Picanha', 20, 450, 4);
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Calorias', N'Carboidratos', N'Fibras', N'Gorduras', N'Nome', N'Proteinas', N'Sodio', N'Tipo') AND [object_id] = OBJECT_ID(N'[TB_Alimentos]'))
+    SET IDENTITY_INSERT [TB_Alimentos] OFF;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240430130616_InitialCreate', N'8.0.4');
+GO
+
+COMMIT;
+GO
+
